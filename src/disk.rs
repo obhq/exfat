140f8425ffa61a62953c2d71657b4ae1586656f3
Putta Khunchalee <ultimaweapon@outlook.com>
2023-03-19 01:55:45 +0700

    Adds no-std supports on disk module

--- src/disk.rs
+++ src/disk.rs
@@ -1,15 +1,43 @@
-use std::error::Error;
-use std::fmt::Display;
+use core::fmt::Display;
 
 /// Encapsulate a disk partition.
 pub trait DiskPartition {
-    fn read(&self, offset: u64, buf: &mut [u8]) -> Result<u64, Box<dyn Error + Send + Sync>>;
+    #[cfg(not(feature = "std"))]
+    fn read(&self, offset: u64, buf: &mut [u8]) -> Result<u64, Box<dyn Display + Send + Sync>>;
 
+    #[cfg(feature = "std")]
+    fn read(
+        &self,
+        offset: u64,
+        buf: &mut [u8],
+    ) -> Result<u64, Box<dyn std::error::Error + Send + Sync>>;
+
+    #[cfg(not(feature = "std"))]
     fn read_exact(
         &self,
         mut offset: u64,
         mut buf: &mut [u8],
-    ) -> Result<(), Box<dyn Error + Send + Sync>> {
+    ) -> Result<(), Box<dyn Display + Send + Sync>> {
+        while !buf.is_empty() {
+            let n = self.read(offset, buf)?;
+
+            if n == 0 {
+                return Err(Box::new(UnexpectedEop));
+            }
+
+            offset += n;
+            buf = &mut buf[n.try_into().unwrap()..];
+        }
+
+        Ok(())
+    }
+
+    #[cfg(feature = "std")]
+    fn read_exact(
+        &self,
+        mut offset: u64,
+        mut buf: &mut [u8],
+    ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
         while !buf.is_empty() {
             let n = self.read(offset, buf)?;
 
@@ -30,9 +58,10 @@ pub trait DiskPartition {
 struct UnexpectedEop;
 
 impl Display for UnexpectedEop {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
         f.write_str("end of partition has been reached")
     }
 }
 
-impl Error for UnexpectedEop {}
+#[cfg(feature = "std")]
+impl std::error::Error for UnexpectedEop {}
